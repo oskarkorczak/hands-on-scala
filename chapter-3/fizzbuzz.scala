9f0bcc18990bd133ee9d72736f1f24cfe03f1e53
Oskar Korczak <o.korczak@gmail.com>
2020-11-07 22:53:45 +0000

    Utilise expression nature of IF

--- chapter-3/fizzbuzz.scala
+++ chapter-3/fizzbuzz.scala
@@ -7,9 +7,11 @@
 */
 
 for (i <- Range.inclusive(0, 100)) {
-  if (i % 3 == 0 && i % 5 == 0) println("FizzBuzz")
-  else if (i % 3 == 0) println("Fizz")
-  else if (i % 5 == 0) println("Buzz")
-  else println(i)
+  println(
+    if (i % 3 == 0 && i % 5 == 0) "FizzBuzz"
+    else if (i % 3 == 0) "Fizz"
+    else if (i % 5 == 0) "Buzz"
+    else i
+  )
 }
 
