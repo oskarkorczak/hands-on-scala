63a9edc96f893c6bb6671125a9e64d5ca8090f92
Oskar Korczak <o.korczak@gmail.com>
2020-11-25 20:53:43 +0000

    Add FizzBuzz pattern matching tuple bool approach

--- chapter-5/fizzbuzz.scala
+++ chapter-5/fizzbuzz.scala
@@ -1,10 +1,28 @@
 
-for (i <- Range.inclusive(0, 100)) {
-  val s = (i % 3, i % 5) match {
-    case (0, 0) => "FizzBuzz"
-    case (0, _) => "Fizz"
-    case (_, 0) => "Buzz"
-    case _ => i
+
+def fizzBuzzTupleInt(): Unit = {
+  for (i <- Range.inclusive(0, 100)) {
+    val s = (i % 3, i % 5) match {
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _ => i
+    }
+    println(s)
   }
-  println(s)
 }
+
+def fizzBuzzTupleBool(): Unit = {
+  for (i <- Range.inclusive(0, 100)) {
+    val s = (i % 3 == 0, i % 5 == 0) match {
+      case (true, true) => "FizzBuzz"
+      case (true, false) => "Fizz"
+      case (false, true) => "Buzz"
+      case (false, false) => i
+    }
+    println(s)
+  }
+}
+
+fizzBuzzTupleInt()
+fizzBuzzTupleBool()
