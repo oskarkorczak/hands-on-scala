e8e19813c2701b0a2b7a1d30c5ad456aab350996
Oskar Korczak <o.korczak@gmail.com>
2020-11-28 22:20:00 +0000

    Add prefixesMatchingString function

--- ch6/trie.scala
+++ ch6/trie.scala
@@ -20,6 +20,17 @@ class Trie() {
     }
     current.exists(_.hasValue)
   }
+
+  def prefixesMatchingString(s: String): Set[Int] = {
+    var current = Option(root)
+    val output = Set.newBuilder[Int]
+    for((c, i) <- s.zipWithIndex if current.nonEmpty) {
+      if (current.get.hasValue) output += i
+      current = current.get.children.get(c)
+    }
+    if(current.exists(_.hasValue)) output += s.length
+    output.result()
+  }
 }
 
   val t = new Trie()
@@ -33,3 +44,7 @@ class Trie() {
   println(s"Has man: ${t.contains("man")}") // true
   println(s"Has mandarin: ${t.contains("mandarin")}") //true
   println(s"Has mandarine: ${t.contains("mandarine")}") //false
+
+
+  println(s"Prefix matching: manible ${t.prefixesMatchingString("manible")}") // Set(3)
+  println(s"Prefix matching: mangosteen ${t.prefixesMatchingString("mangosteen")}") // Set(3, 5)
