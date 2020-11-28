29d588cd07c7c23a40d96b9dc2c02f6c1584e4fb
Oskar Korczak <o.korczak@gmail.com>
2020-11-28 11:24:55 +0000

    Make merge sort more generic

--- ch6/mergeSort.scala
+++ ch6/mergeSort.scala
@@ -1,18 +1,18 @@
 // Merge sort implementation
 
-def mergeSort(a: Array[Int]): Array[Int] = {
+def mergeSort[T: Ordering](a: IndexedSeq[T]): IndexedSeq[T] = {
   if (a.length <= 1) a
   else {
     val halfSize: Int = a.length / 2
     val (left, right) = a.splitAt(halfSize)
     val (sortedLeft, sortedRight) = (mergeSort(left), mergeSort(right))
     var (leftIdx, rightIdx) = (0, 0)
-    val output = Array.newBuilder[Int]
+    val output = IndexedSeq.newBuilder[T]
     while (leftIdx < sortedLeft.length || rightIdx < sortedRight.length) {
       val takeLeft = (leftIdx < sortedLeft.length, rightIdx < sortedRight.length) match {
         case (true, false) => true
         case (false, true) => false
-        case (true, true) => sortedLeft(leftIdx) < sortedRight(rightIdx)
+        case (true, true) => Ordering[T].lt(sortedLeft(leftIdx), sortedRight(rightIdx))
       }
       if (takeLeft) {
         output += sortedLeft(leftIdx)
@@ -29,5 +29,7 @@ def mergeSort(a: Array[Int]): Array[Int] = {
 // try to run mergeSort from amm console
 // amm --predef mergeSort.scala
 val a = Array(4, 0, 1, 5, 3, 2)
+val v = Vector(5, 3, 4, 2, 1)
+val s = Vector("banana", "apple", "durian", "cabbage")
 val res = mergeSort(a)
 println(res)
